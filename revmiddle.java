c0d76e4985d6da67ad1b5ef8f6487e6dd6026848
Nandan Nayak <nandannayak2102@gmail.com>
2026-01-01 06:44:15 +0530

    hi

---
+++ revmiddle.java
@@ -0,0 +1,46 @@
+package fr;
+class revmiddle {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    static void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        printList(head);
+        head = reverse(head);
+        printList(head);
+    }
+}
